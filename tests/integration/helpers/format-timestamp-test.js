6629790fcf407c5931ad7c9265eac79fd1f80487
John Joseph <atiquajohn@gmail.com>
2020-01-11 21:51:06 +0700

    added helper funciton and unit test

---
+++ tests/integration/helpers/format-timestamp-test.js
@@ -0,0 +1,24 @@
+import {
+  module,
+  test
+} from 'qunit';
+import {
+  setupRenderingTest
+} from 'ember-qunit';
+import {
+  render
+} from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Helper | format-timestamp', function (hooks) {
+  setupRenderingTest(hooks);
+
+  // Replace this with your real tests.
+  test('it renders', async function (assert) {
+    this.set('mydate', '04-05-1983');
+
+    await render(hbs `{{format-timestamp mydate}}`);
+
+    assert.equal(this.element.textContent.trim(), "Apr 5, 1983 00:00.00 AM");
+  });
+});
