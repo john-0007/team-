48a8e3dd6ced8ad95cd5ceca28a5dd50f406534c
John Joseph <atiquajohn@gmail.com>
2020-01-12 23:10:11 +0700

    added state action and decorator

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,8 +1,35 @@
 import Component from '@glimmer/component';
+import {
+  action
+} from '@ember/object'
+import {
+  tracked
+} from '@glimmer/tracking'
 
 export default class LoginFormComponent extends Component {
+  @tracked
+  userId = null
 
+  get isDisabled() {
+    return !this.userId
+  }
+
+  loginAsUserWithId(val) {
+    console.log(val)
+  }
+
+  @action
+  onSelectChanged(evt) {
+    this.userId = evt.target.value;
+  }
+
+  @action
   onLoginFormSubmit(evt) {
-    debugger
+    evt.preventDefault()
+    const {
+      target
+    } = evt
+    const val = target.querySelector('select').value
+    this.loginAsUserWithId(val)
   }
 }
--- tests/integration/components/login-form-test.js
+++ tests/integration/components/login-form-test.js
@@ -1,26 +1,31 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import {
+  module,
+  test
+} from 'qunit';
+import {
+  setupRenderingTest
+} from 'ember-qunit';
+import {
+  render
+} from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | login-form', function(hooks) {
+module('Integration | Component | login-form', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<LoginForm />`);
+    await render(hbs `<LoginForm />`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.deepEqual(this.element.textContent.trim().replace(/\s*\n+\s*/g, '\n').split('\n'), [
+      'Login',
+      'Select a user',
+      'Testy Testerson',
+      'Sample McData'
+    ]);
 
-    // Template block usage:
-    await render(hbs`
-      <LoginForm>
-        template block text
-      </LoginForm>
-    `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
   });
 });
